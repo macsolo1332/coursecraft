9425ffc90518f44544bc04622e6fea6158c2e559
macsolo1332 <akashsuresh1332@gmail.com>
2024-02-27 13:21:45 +0530

    creating prisma database with NEON

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,25 +1,38 @@
 import { useRouter, useSearchParams } from 'next/navigation';
-import { trpc } from '../_trpc/client';
 import { useEffect } from 'react';
+import { trpc } from '../_trpc/client';
+import { Loader2 } from 'lucide-react';
+
 
 const Page = () => {
     const router = useRouter();
     const searchParams = useSearchParams();
     const origin = searchParams.get('origin');
 
-    const { data, isLoading } = trpc.authCallback.useQuery(undefined);
-
     useEffect(() => {
-        if (data?.success) {
-            // user is synced to db
+        const { data, error } = trpc.authCallback.useQuery(undefined, {
+            retry: true,
+            retryDelay: 500,
+        });
+
+        if (error) {
+            if (error.data?.code === 'UNAUTHORIZED') {
+                router.push('/sign-in');
+            }
+        } else if (data && data.success) {
             router.push(origin ? `/${origin}` : '/dashboard');
         }
-    }, [data, router, origin]);
+    }, [router, origin]);
 
-    // You can render your component contents here
     return (
-        <div>
-            {/* Your component contents */}
+        <div className='w-full mt-24 flex justify-center'>
+            <div className='flex flex-col items-center gap-2'>
+                <Loader2 className='h-8 w-8 animate-spin text-zinc-800' />
+                <h3 className='font-semibold text-xl'>
+                    Setting up your account...
+                </h3>
+                <p>You will be redirected automatically.</p>
+            </div>
         </div>
     );
 };
